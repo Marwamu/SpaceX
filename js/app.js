afdb691958d00b2f90e5404607fd796b7f0ff6cd
Marwamu <marwamohamed1293@gmail.com>
2023-11-26 00:58:33 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
-const btn = document.getElementById('menu-btn')
-const overlay = document.getElementById('overlay')
-const menu = document.getElementById('mobile-menu')
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
 
-btn.addEventListener('click', navToggle)
+btn.addEventListener("click", navToggle);
 
-function navToggle(){
-    btn.classList.toggle('open')
-    overlay.classList.toggle('overlay-show')
-    document.body.classList.toggle('stop-scrolling')
-    menu.classList.toggle('show-menu')
+function navToggle() {
+  btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+  document.body.classList.toggle("stop-scrolling");
+  menu.classList.toggle("show-menu");
 }
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerHTML = "0";
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      console.log(target);
+      counter.innerHTML = target;
+    };
+    updateCounter();
+    
+  });
+}
+
+countUp();
