c14f41ed08266dff3ecdbebdeea9f0110b5cff42
Marwamu <marwamohamed1293@gmail.com>
2023-12-11 16:48:44 +0200

    Falcon heavy

--- js/app.js
+++ js/app.js
@@ -2,8 +2,14 @@ const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
 const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
 
 btn.addEventListener("click", navToggle);
+document.addEventListener('scroll', scrollPage)
+
+
+
+
 
 function navToggle() {
   btn.classList.toggle("open");
@@ -12,18 +18,57 @@ function navToggle() {
   menu.classList.toggle("show-menu");
 }
 
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  console.log(scrollPos)
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  }
+  else if(scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+  else {
+    
+  }
+}
+
 function countUp() {
   counters.forEach((counter) => {
     counter.innerHTML = "0";
 
     const updateCounter = () => {
+      //Get Count Target
       const target = +counter.getAttribute("data-target");
-      console.log(target);
-      counter.innerHTML = target;
+
+      //Get Current Counter value
+      const c = +counter.innerHTML;
+
+      //Create an increment
+      const increment = target / 100;
+
+      // If conter is less than traget, add increment 
+      if (c < target) {
+        //Round up and set counter value
+        counter.innerHTML = `${Math.ceil(c + increment)}`
+
+        setTimeout(updateCounter, 75);
+      }
+      else {
+        counter.innerHTML = target;
+      }
+
     };
     updateCounter();
     
   });
 }
 
-countUp();
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = '0'));
+}
+
+scrollPage();
